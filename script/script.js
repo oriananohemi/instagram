bc9fd6c0f9558865a26f60b0910c4d0236f14b41
Oriana Reina <oriana.nohemi22@gmail.com>
2020-04-09 11:37:01 -0500

    Traer las imagenes desde un json

--- script/script.js
+++ script/script.js
@@ -1,3 +1,34 @@
+const loadJSON = callback => {
+  let xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", "../images.json", true);
+  xobj.onreadystatechange = () => {
+    if (xobj.readyState === 4 && xobj.status === 200) {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+};
+
+loadJSON(res => {
+  const data = JSON.parse(res);
+  loadImg(data);
+});
+
+const loadImg = data => {
+  const pathRoot = data.path_root;
+  const image = data.images;
+  const container = document.getElementById("collection");
+  for (i = 0; i < image.length; i++) {
+    const img = document.createElement("img");
+    const name = image[i].title;
+    const path = `${pathRoot}${image[i].name}`;
+    img.setAttribute("src", path);
+    img.setAttribute("class", "img");
+    container.appendChild(img);
+  }
+};
+
 function cargar() {
   const collections = document.querySelectorAll("#collection > img");
   collections.forEach(collection => collection.addEventListener("click", show));
@@ -8,8 +39,8 @@ cargar();
 let modal = document.getElementById("modal");
 
 function show(evento) {
-  let img = evento.target;
-  let copyimg = img.cloneNode(true);
+  let image = evento.target;
+  let copyimg = image.cloneNode(true);
   modal.appendChild(copyimg);
   modal.classList.remove("hidden");
 }
@@ -20,4 +51,10 @@ function hidden() {
   let copyimg = modal.getElementsByClassName("img")[0];
   modal.removeChild(copyimg);
   modal.classList.add("hidden");
+}
+
+document.getElementById("behind").addEventListener("click", behind);
+
+function behind(evento) {
+  console.log(evento.target);
 }
